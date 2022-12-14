31c87b7ea11220a63a8ffb4f234aab8fe790ee4a
Kazuki Taira <kazuki@lift99.co>
2022-12-14 13:24:24 +0200

    ability to generate ascii art in the backend

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+
+	"github.com/common-nighthawk/go-figure"
+
+	"bufio"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// Create a text file
+	f, err := os.Create("result.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer f.Close()
+
+	// Printing ascii art
+	myFigure := figure.NewFigure("Hello hello", "", true)
+	myFigure.Print()
+
+	datawriter := bufio.NewWriter(f)
+	for _, data := range myFigure.Slicify() {
+		_, _ = datawriter.WriteString(data + "\n")
+	}
+	datawriter.Flush()
+
+	// Print a content of result.txt to the console
+	content, err := ioutil.ReadFile("result.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(content))
+
+	// TODO: Send a string to the frontend??
+
+	// Server with gin
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Kazut8492/ascii-art-generator
 go 1.19
 
 require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
