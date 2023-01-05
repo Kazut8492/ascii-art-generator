23d87137681f3a28918244819b52c6f08fa187ed
Kazuki Taira <kazuki@lift99.co>
2023-01-05 20:47:39 +0200

    fixed margin issue

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -36,28 +36,14 @@ const Home: React.FC = () => {
         .catch(error=>console.log(error))
     }
 
-    // Component to convert line breaks to <br /> tags
-    const MultiLineBody = ({ body }: { body: string }) => {
-        const texts = body.split('\n').map((item, index) => {
-          return (
-            <React.Fragment key={index}>
-              {item}
-              <br />
-            </React.Fragment>
-          );
-        });
-        return <div>{texts}</div>;
-      };
-
     return (<>
         <Form onSubmit={handleFormSubmit}>
-            <Form.Group className='mb-3' controlId='asciiInput'>
+            <Form.Group className='col-md-5' controlId='asciiInput'>
                 <Form.Label>Type in what you want to convert into ascii art</Form.Label>
                 <Form.Control type='text' placeholder='type in here' value={input} onChange={handleFormChange}/>
             </Form.Group>
-            <Button type='submit'>Submit</Button>
+            <Button className='mt-1' type='submit'>Submit</Button>
         </Form>
-        {/* {output && <MultiLineBody body={output} />} */}
         {output && <p className='ascii-output'>{output}</p>}
     </>)
 }
